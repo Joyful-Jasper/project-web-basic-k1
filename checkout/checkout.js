0d04dd987418857aa70db6f9653fd0a1059851e4
abrnsyh <abriansyah1099@gmail.com>
2022-06-26 04:48:46 +0700

    Add checkout fiture

---
+++ checkout/checkout.js
@@ -0,0 +1,84 @@
+import { lazyLoad, getParamsFromUrl, symbols as sym } from '../common.js';
+import { itemData } from '../assets/dummy.js';
+
+const form = document.getElementById("form");
+const firstName = document.getElementById("firstname");
+const lastName = document.getElementById("lastname");
+const phone = document.getElementById("phone");
+const nik = document.getElementById("nik");
+
+// event listener
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    checkData();
+    if (checkData() === true){
+        window.location.href = "http://google.com";
+    }
+});
+
+
+
+// Check data di form
+const checkData = () => {
+    const firstNameValue = firstName.value.trim();
+    const lastNameValue = lastName.value.trim();
+    const phoneValue = phone.value.trim();
+    const nikValue = nik.value.trim();
+
+    let firstNameValid = false;
+    let lastNameValid = false;
+    let phoneValid = false;
+    let nikValid = false;
+
+    if(firstNameValue === ''){
+        setErrorFor(firstName);
+    } else {
+        setSuccessFor(firstName);
+        firstNameValid = true;
+    }
+
+    if(lastNameValue === ''){
+        setErrorFor(lastName);
+    } else {
+        setSuccessFor(lastName);
+        lastNameValid = true;
+    }
+
+    if(phoneValue === '' || phoneValue === isNaN || phoneValue.length < 11 || phoneValue.length > 12){
+        setErrorFor(phone);
+    } else {
+        setSuccessFor(phone);
+        phoneValid = true;
+    }
+
+    if(nikValue === '' || nikValue === isNaN || nikValue.length < 16 || nikValue.length > 16){
+        setErrorFor(nik);
+    } else {
+        setSuccessFor(nik);
+        nikValid = true;
+    }
+
+    if (!firstNameValid || !lastNameValid || !nikValid || !phoneValid) {
+        return false
+    } else {
+        return true
+    }
+}
+
+const getData = () => {
+    return {firstNameValue : firstName.value.trim(),
+            lastNameValue : lastName.value.trim(),
+            phoneValue : phone.value.trim(),
+            nikValue : nik.value.trim()}
+
+}
+
+// memunculkan pesan error
+const setErrorFor = (x) => {
+    x.className += " is-invalid";
+}
+
+// memunculkan pesan sukses 'is-valid' 
+const setSuccessFor = (x) => {
+    x.className = "form-control is-valid";
+}
